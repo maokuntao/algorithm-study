964be9728ea27f30bcce2422559dc8f3f5a98c67
taomaokun@MacBookPro <taomaokun@foxmail.com>
2017-04-20 16:01:15 +0800

    add comment

--- src/com/taomk/sort/bubble/BubbleSort.java
+++ src/com/taomk/sort/bubble/BubbleSort.java
@@ -7,10 +7,10 @@ import com.taomk.sort.common.ArrayUtils;
 /**
  * <pre>
  * 冒泡排序：
- * 
- * 	基本思想：针对数列中每一个元素，依次比较大小，如果顺序错误的话，就将这两个元素的值交换位置。
- * 	复杂度：O(N^2)
+ * 基本思想：针对数列中每一个元素，依次比较大小，如果顺序错误的话，就将这两个元素的值交换位置。
+ * 复杂度：O(N^2)
  * </pre>
+ * 
  * @author taomk 2017年4月20日 下午1:40:21
  *
  */
@@ -18,41 +18,82 @@ public class BubbleSort {
 
 	private static AtomicInteger loopCount = new AtomicInteger(0);
 	private static long interval = 0L;
-	
-	public static void sort(int[] unsortedArrays){
-		
+
+	/**
+	 * <pre>
+	 * 两层循环，每层循环N次。
+	 * 每N次外层循环结束，确保前N个元素是有序的。
+	 * 两层循环全部结束，则全部有序。
+	 * </pre>
+	 * 
+	 * @param unsortedArrays
+	 */
+	public static void sort1(int[] unsortedArrays) {
+
 		Long startTime = System.nanoTime();
-		
+
 		int temp;
-		int length = unsortedArrays.length ;
-		
+		int length = unsortedArrays.length;
+
 		for (int i = 0; i < length; i++) {
-			for(int j = 0; j < length; j++){
+			for (int j = 0; j < length; j++) {
 				loopCount.getAndIncrement();
-				if(unsortedArrays[i] < unsortedArrays[j]){
+				if (unsortedArrays[i] < unsortedArrays[j]) {
 					temp = unsortedArrays[i];
 					unsortedArrays[i] = unsortedArrays[j];
 					unsortedArrays[j] = temp;
 				}
 			}
-			System.out.print("Loop" + loopCount.get() + " : " );
+			System.out.print("Loop" + loopCount.get() + " : ");
 			ArrayUtils.displayDetails(unsortedArrays);
 		}
-		
+
 		interval = System.nanoTime() - startTime;
 	}
-	
+
+	/**
+	 * <pre>
+	 * sort1 的改进型：
+	 * 比较的是相邻两个元素，如果顺序错误，则交换位置。
+	 * 外层一次循环之后，最大的那个元素就在最后（如果按照从小到大排列的话），
+	 * 下次循环时，就只需要在在剩下的那些元素中按照这个规则继续进行即可。
+	 * 
+	 * </pre>
+	 * @param unsortedArrays
+	 */
+	public static void sort2(int[] unsortedArrays) {
+		int temp = 0;
+		int size = unsortedArrays.length;
+		for (int i = 0; i < size - 1; i++) {
+			for (int j = 0; j < size - 1 - i; j++) {
+				if (unsortedArrays[j] > unsortedArrays[j + 1]) {
+					loopCount.getAndIncrement();
+					temp = unsortedArrays[j];
+					unsortedArrays[j] = unsortedArrays[j + 1];
+					unsortedArrays[j + 1] = temp;
+				}
+			}
+			System.out.print("Loop" + loopCount.get() + " : ");
+			ArrayUtils.displayDetails(unsortedArrays);
+		}
+		Long startTime = System.nanoTime();
+		interval = System.nanoTime() - startTime;
+	}
+
 	public static void main(String[] args) {
 		int[] unsortedArrays = ArrayUtils.genaralInteger(10, 100);
-		System.out.println("未排序之前：");
+		System.out.print("未排序之前：");
 		ArrayUtils.displayDetails(unsortedArrays);
+
+		System.out.println("排序过程：");
+		sort1(unsortedArrays);
+//		sort2(unsortedArrays);
 		
-		System.out.println("排序之后：");
-		sort(unsortedArrays);
+		System.out.print("排序之后：");
 		ArrayUtils.displayDetails(unsortedArrays);
-		
+
 		System.out.println("循环次数：" + loopCount.get());
 		System.out.println("排序耗时：" + interval + "ns");
 	}
-	
+
 }
